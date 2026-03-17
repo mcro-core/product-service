2390ed24feb2143963bdf144cb6ff3e298278f2b
inosh611 <inoshharsha531@gmail.com>
2026-03-17 10:14:50 +0530

    update base entity

--- src/main/java/com/micro_core/product_service/entity/BaseEntity.java
+++ src/main/java/com/micro_core/product_service/entity/BaseEntity.java
@@ -1,17 +1,20 @@
 package com.micro_core.product_service.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreatedDate
